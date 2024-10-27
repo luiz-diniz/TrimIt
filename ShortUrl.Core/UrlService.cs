fcd267c4399bb47207ee9ba69fa9625e2f0e6b23
Luiz Diniz <luiz.gustavon@hotmail.com>
2024-10-27 15:34:18 -0300

    - Logs inclusion

--- ShortUrl.Core/UrlService.cs
+++ ShortUrl.Core/UrlService.cs
@@ -16,6 +16,7 @@ namespace ShortUrl.Core
         private readonly ILogger _logger;
         private readonly IUrlRepository _urlRepository;
         private readonly IMemoryCache _cache;
+        private readonly TimeSpan _expirationCacheTime = TimeSpan.FromMinutes(30);
 
         public UrlService(ILogger<UrlService> logger, IUrlRepository urlRepository, IMemoryCache cache)
         {
@@ -36,15 +37,26 @@ namespace ShortUrl.Core
                 {
                     var shortUrl = CreateShortUrl(url.Url);
 
+                    _logger.LogDebug("Short URL created '{shortUrl}' for URL '{url}'.", shortUrl, url.Url);
+
                     if (_urlRepository.GetUrl(shortUrl) is not null)
+                    {
+                        _logger.LogDebug("Short URL '{shortUrl}' already found in the database. Another Short URL will be created.", shortUrl);
                         continue;
+                    }
+
+                    _logger.LogDebug("Short URL '{shortUrl}' not found in the database, proceeding with persistence flow.", shortUrl);
 
                     urlEntity.ShortUrl = shortUrl;
                 }
 
                 _urlRepository.Create(urlEntity);
 
-                _cache.Set(urlEntity.ShortUrl, urlEntity.OriginalUrl, TimeSpan.FromMinutes(30));
+                _logger.LogDebug("Short URL '{shortUrl}' created in the database.", urlEntity.ShortUrl);
+
+                _cache.Set(urlEntity.ShortUrl, urlEntity.OriginalUrl, _expirationCacheTime);
+
+                _logger.LogDebug("Short URL '{shortUrl}' added in the cache.", urlEntity.ShortUrl);
 
                 return urlEntity.ShortUrl;
             }
@@ -66,18 +78,36 @@ namespace ShortUrl.Core
 
                 if (cachedUrl is null)
                 {
+                    _logger.LogDebug("Short URL '{shortUrl}' not found in the cache.", shortUrl);
+
                     var url = _urlRepository.GetUrl(shortUrl);
 
                     if(url is not null)
                     {
+                        _logger.LogDebug("Short URL '{shortUrl}' found in the database.", shortUrl);
+
                         _urlRepository.UpdateClicks(shortUrl);
-                        _cache.Set(shortUrl, url, TimeSpan.FromMinutes(30));
+
+                        _logger.LogDebug("Short URL '{shortUrl}' clicks updated.", shortUrl);
+
+                        _cache.Set(shortUrl, url, _expirationCacheTime);
+
+                        _logger.LogDebug("Short URL '{shortUrl}' added in the cache with expiration in {expirationTime} minutes.", shortUrl, _expirationCacheTime.Minutes);
+
+                        return url;
                     }
+
+                    _logger.LogDebug("Short URL '{shortUrl}' not found in the database.", shortUrl);
 
                     return url;
                 }
 
+                _logger.LogDebug("Short URL '{shortUrl}' found in the cache.", shortUrl);
+
                 _urlRepository.UpdateClicks(shortUrl);
+
+                _logger.LogDebug("Short URL '{shortUrl}' clicks updated.", shortUrl);
+
                 return cachedUrl.ToString();
             }
             catch (Exception ex)
